53e929c8cc24505fbd2e06b7013bed67ec8ee1c1
Code Plus Coffee <m416a@protonmail.com>
2020-09-23 14:55:25 +0530

    rm obsolete code.

--- server/source/routes/admin/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const adminRouter = express.Router();
-
-adminRouter.get('/', function (req, res) {
-    res.status(200).json({ message: 'Hey, this is an admin route!' });
-});
-
-module.exports = adminRouter;
