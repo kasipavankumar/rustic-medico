0114f821dc4f2aca04104fb0948b9a14fa399026
Code Plus Coffee <m416a@protonmail.com>
2020-09-25 22:27:28 +0530

    change to arrow function.

--- server/source/server.js
+++ server/source/server.js
@@ -5,6 +5,4 @@ dotenv.config();
 
 const __PORT__ = process.env.PORT || 5000;
 
-app.listen(__PORT__, function () {
-    console.log(`App listening on port ${__PORT__}`);
-});
+app.listen(__PORT__, () => console.log(`App listening on port ${__PORT__}`));
