ae125bd47d0cc4076a126dd91997970670c07a22
Code Plus Coffee <m416a@protonmail.com>
2020-09-26 00:32:04 +0530

    gracefully check env vars

--- server/source/utils/config.js
+++ server/source/utils/config.js
@@ -6,4 +6,12 @@ const isProduction = process.env.NODE_ENV === 'production';
 const ADMIN_KEY = fromEnv('ADMIN_KEY') || '$up3r$3cr3tAdm1n';
 const DATABASE_URL = isProduction ? fromEnv('DATABASE_URL') : fromEnv('DATABASE_URL_DEV');
 
+if (!ADMIN_KEY) {
+    throw new Error('no ADMIN_KEY found in .env');
+}
+
+if (!DATABASE_URL) {
+    throw new Error('no DATABASE_URL found in .env');
+}
+
 module.exports = { DATABASE_URL, ADMIN_KEY };
