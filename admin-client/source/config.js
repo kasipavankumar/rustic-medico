208252682840066b2d1e44e999fa208cd6789b1f
Code Plus Coffee <m416a@protonmail.com>
2020-09-27 15:52:29 +0530

    add env vars.

--- admin-client/source/config.js
+++ admin-client/source/config.js
@@ -1,6 +1,8 @@
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
-const API_URL_DEVELOPMENT = `http://localhost:5000`;
-const API_URL_PRODUCTION = `https://rustic-medico-api.herokuapp.com`;
-
+const API_URL_DEVELOPMENT = process.env.NEXT_PUBLIC_API_URL_DEVELOPMENT;
+const API_URL_PRODUCTION = process.env.NEXT_PUBLIC_API_URL_PRODUCTION;
 export const API_URL = isDevelopment ? API_URL_DEVELOPMENT : API_URL_PRODUCTION;
+
+// Admin Key
+export const ADMIN_KEY = process.env.NEXT_PUBLIC_ADMIN_KEY;
