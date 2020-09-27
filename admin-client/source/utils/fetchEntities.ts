5ca95716dc16ba116cc341ab53056a033d550ca8
Code Plus Coffee <m416a@protonmail.com>
2020-09-28 00:23:06 +0530

    utils(fetch): common fetch util

---
+++ admin-client/source/utils/fetchEntities.ts
@@ -0,0 +1,34 @@
+import Axios from 'axios';
+import { ADMIN_KEY, API_URL } from '../config';
+
+type EntityLike = 'drugs' | 'drug_manufacturers' | 'employees' | 'doctors' | 'customers';
+
+interface FetchEntityResult {
+    entityData?: any[];
+    hasErrors: boolean;
+    errors?: Error;
+}
+
+export default async function fetchEntity(entity: EntityLike): Promise<FetchEntityResult> {
+    try {
+        const response = await Axios({
+            method: 'GET',
+            url: `${API_URL}/api/admin/${entity}/get/all`,
+            headers: {
+                'Content-Type': 'application/json',
+                'Admin-Key': ADMIN_KEY,
+            },
+        });
+
+        return {
+            entityData: response.data[entity],
+            hasErrors: false,
+        };
+    } catch (err) {
+        console.error(err);
+        return {
+            hasErrors: true,
+            errors: err,
+        };
+    }
+}
---
+++ admin-client/source/utils/index.ts
@@ -0,0 +1,1 @@
+export { default as fetchEntities } from './fetchEntities';
