cf216f38d31440c20cc0c3d1a2c3f6aa2e851ef4
Code Plus Coffee <m416a@protonmail.com>
2020-09-28 00:23:21 +0530

    add header to layout

--- admin-client/source/components/Layout/Layout.jsx
+++ admin-client/source/components/Layout/Layout.jsx
@@ -1,9 +1,13 @@
+import Header from '../Header';
 import styles from './Layout.module.scss';
 
-const Layout = ({ children }) => {
+const Layout = ({ children, path }) => {
     return (
         <>
-            <div className={styles.container}>{children}</div>
+            <div className={styles.container}>
+                <Header path={path} />
+                {children}
+            </div>
         </>
     );
 };
