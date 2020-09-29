df54f6fe4ac4520712cf5432d50dba3f305050f9
Code Plus Coffee <m416a@protonmail.com>
2020-09-29 20:45:34 +0530

    add material data grid.

--- admin-client/pages/drugs/index.jsx
+++ admin-client/pages/drugs/index.jsx
@@ -8,9 +8,12 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import { DataGrid } from '@material-ui/data-grid';
 import { Layout, SEO } from '../../source/components';
 import { DrugsCreationDialog } from '../../source/components/EntityCreationDialogs';
 import fetchEntities from '../../source/utils/fetchEntities';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const useStyles = makeStyles((theme) => ({
     table: {
@@ -43,10 +46,44 @@ const useStyles = makeStyles((theme) => ({
     noDataSubtitle: {
         marginTop: 10,
     },
+    dataGridRoot: {
+        height: '565px',
+        width: '100%',
+    },
 }));
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'name', headerName: 'Name', width: 200 },
+    { field: 'price', headerName: 'Price (INR)', width: 150 },
+    { field: 'medical_description', headerName: 'Medical Description', width: 350 },
+    { field: 'manufacturing_date', headerName: 'Manufacturing Date', width: 200 },
+    { field: 'expiry_date', headerName: 'Expiry Date', width: 200 },
+    { field: 'manufacturer_name', headerName: 'Manufacturer', width: 300 },
+    { field: 'supplier_name', headerName: 'Supplier', width: 250 },
+    { field: 'created_at', headerName: 'Created At', width: 150 },
+    { field: 'updated_at', headerName: 'Updated At', width: 150 },
+];
 
 const AllDrugs = ({ drugs, errors }) => {
     const classes = useStyles();
+
+    const rows = drugs.map((drug, i) => {
+        const { name, price, medical_description, manufacturing_date, expiry_date, manufacturer_name, supplier_name, created_at, updated_at } = drug;
+
+        return {
+            id: i + 1,
+            name,
+            price,
+            medical_description,
+            manufacturing_date: new Date(manufacturing_date).toDateString(),
+            expiry_date: new Date(expiry_date).toDateString(),
+            manufacturer_name,
+            supplier_name,
+            created_at: new Date(created_at).toDateString(),
+            updated_at: new Date(updated_at).toDateString(),
+        };
+    });
 
     if (errors) {
         return (
@@ -87,15 +124,19 @@ const AllDrugs = ({ drugs, errors }) => {
 
             <DrugsCreationDialog />
 
-            <TableContainer component={Paper}>
+            {/* <TableContainer component={Paper}>
                 <Table stickyHeader className={classes.table} aria-label="Drugs data table">
                     <TableHead>
                         <TableRow>
                             <TableCell>Name</TableCell>
                             <TableCell>Price</TableCell>
+                            <TableCell>Medical Description</TableCell>
+                            <TableCell>Manufacturing Date</TableCell>
                             <TableCell>Expiry Date</TableCell>
                             <TableCell>Manufacturer</TableCell>
                             <TableCell>Supplier</TableCell>
+                            <TableCell>Created At</TableCell>
+                            <TableCell>Updated At</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -105,14 +146,22 @@ const AllDrugs = ({ drugs, errors }) => {
                                     {drug.name}
                                 </TableCell>
                                 <TableCell>{`₹ ${drug.price}`}</TableCell>
+                                <TableCell>{drug.medical_description}</TableCell>
+                                <TableCell>{new Date(drug.manufacturing_date).toDateString()}</TableCell>
                                 <TableCell>{new Date(drug.expiry_date).toDateString()}</TableCell>
                                 <TableCell>{drug.manufacturer_name}</TableCell>
                                 <TableCell>{drug.supplier_name}</TableCell>
+                                <TableCell>{new Date(drug.created_at).toDateString()}</TableCell>
+                                <TableCell>{new Date(drug.updated_at).toDateString()}</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
-            </TableContainer>
+            </TableContainer> */}
+
+            <div className={classes.dataGridRoot}>
+                <DataGrid rows={rows} columns={columns} pageSize={8} />
+            </div>
         </Layout>
     );
 };
