0c4f856a2595f38a1c23c86920ec1264fed0d094
Code Plus Coffee <m416a@protonmail.com>
2020-10-01 22:37:02 +0530

    add accept header.

--- admin-client/source/utils/fetchEntities.ts
+++ admin-client/source/utils/fetchEntities.ts
@@ -4,31 +4,31 @@ import { ADMIN_KEY, API_URL } from '../config';
 export type EntityLike = 'drugs' | 'manufacturers' | 'employees' | 'doctors' | 'customers' | 'suppliers';
 
 interface FetchEntityResult {
-    entityData?: any[];
-    hasErrors: boolean;
-    errors?: Error;
+  entityData?: any[];
+  hasErrors: boolean;
+  errors?: Error;
 }
 
 export default async function fetchEntity(entity: EntityLike): Promise<FetchEntityResult> {
-    try {
-        const response = await Axios({
-            method: 'GET',
-            url: `${API_URL}/api/admin/${entity}/get/all`,
-            headers: {
-                'Content-Type': 'application/json',
-                'Admin-Key': ADMIN_KEY,
-            },
-        });
+  try {
+    const response = await Axios({
+      method: 'GET',
+      url: `${API_URL}/api/admin/${entity}/get/all`,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Admin-Key': ADMIN_KEY,
+      },
+    });
 
-        return {
-            entityData: response.data,
-            hasErrors: false,
-        };
-    } catch (err) {
-        console.error(err);
-        return {
-            hasErrors: true,
-            errors: err,
-        };
-    }
+    return {
+      entityData: response.data,
+      hasErrors: false,
+    };
+  } catch (err) {
+    return {
+      hasErrors: true,
+      errors: err,
+    };
+  }
 }
