a30d11cc8860b6ec468e5f87904cd3a48254b728
Code Plus Coffee <m416a@protonmail.com>
2020-10-05 21:23:20 +0530

    improve token verification.

--- server/source/middleware/verifyToken.js
+++ server/source/middleware/verifyToken.js
@@ -1,29 +1,57 @@
 const jwt = require('jsonwebtoken');
 const { StatusCodes, ReasonPhrases } = require('http-status-codes');
+
 const { JWT_SECRET } = require('../utils/config');
 
+/**
+ * Middleware function to verify incoming JWT tokens
+ * attached by cookieParser.
+ *
+ * @param {Request} req Express Request
+ * @param {Response} res Express Response
+ * @param {import('express').NextFunction} next Next middleware in queue
+ */
 const verifyToken = (req, res, next) => {
   try {
-    // const token = req.headers.authorization.split(' ')[1];
-    const token = req.cookies['_SID_'];
-
+    /**
+     * cookieParser will attach all incoming cookies to the req.cookies object.
+     */
+    const token = req.cookies['_SID_'] || null;
+    /**
+     * Token is not present.
+     */
     if (!token) {
-      return res.status(StatusCodes.UNAUTHORIZED).json({ msg: ReasonPhrases.UNAUTHORIZED });
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ msg: ReasonPhrases.UNAUTHORIZED });
     }
-
-    const user = jwt.verify(token, JWT_SECRET);
-
-    res.locals['user'] = user;
-
+    /**
+     * Token is recieved, verify it with SECRET.
+     */
+    jwt.verify(token, JWT_SECRET);
+    /**
+     * Token verified, call next middleware.
+     */
     next();
   } catch (err) {
-    console.log(err);
-
-    if (err instanceof jwt.TokenExpiredError || err instanceof jwt.JsonWebTokenError) {
-      return res.status(StatusCodes.UNAUTHORIZED).json({ error: ReasonPhrases.UNAUTHORIZED });
+    /**
+     * If JWT is not attached or has expired,
+     * api should respond with UNAUTHORIZED.
+     */
+    if (
+      err instanceof jwt.TokenExpiredError ||
+      err instanceof jwt.JsonWebTokenError
+    ) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ error: ReasonPhrases.UNAUTHORIZED });
     }
-
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: ReasonPhrases.INTERNAL_SERVER_ERROR });
+    /**
+     * Something went wrong with server.
+     */
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: ReasonPhrases.INTERNAL_SERVER_ERROR });
   }
 };
 
