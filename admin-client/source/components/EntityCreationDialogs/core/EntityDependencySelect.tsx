f56e24a1a711a3d52eca2e2f18bc823eeba7bea8
Code Plus Coffee <m416a@protonmail.com>
2020-10-07 23:24:27 +0530

    use hooks to fetch entity data.

--- admin-client/source/components/EntityCreationDialogs/core/EntityDependencySelect.tsx
+++ admin-client/source/components/EntityCreationDialogs/core/EntityDependencySelect.tsx
@@ -1,32 +1,21 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 import { InputLabel, Select, MenuItem } from '@material-ui/core';
 
 import { FormControl } from './DialogBase';
 import { IEntityDependentsSelectionProps } from './types';
-import NetworkService from '../../../services/networkService';
+import useEntityData from 'hooks/useEntityData';
 
-const EntityDenpendencySelect: FC<IEntityDependentsSelectionProps> = ({ entityName, label, name, value, variant, onChange }) => {
-  const networkService = new NetworkService(entityName);
-  const [entityData, setEntityData] = useState<any[] | boolean>([]);
+const EntityDenpendencySelect: FC<IEntityDependentsSelectionProps> = ({
+  entityName,
+  label,
+  name,
+  value,
+  variant,
+  onChange,
+}) => {
+  const { hasErrors, isLoading, entityData } = useEntityData(entityName);
 
-  useEffect(() => {
-    const fetch = async () => {
-      try {
-        const data = await networkService.fetchData();
-        setEntityData(data);
-      } catch (err) {
-        throw new Error(err);
-      }
-    };
-
-    fetch();
-
-    return () => {
-      setEntityData([]);
-    };
-  }, []);
-
-  if (!entityData) {
+  if (hasErrors || isLoading) {
     return <></>;
   }
 
@@ -35,12 +24,11 @@ const EntityDenpendencySelect: FC<IEntityDependentsSelectionProps> = ({ entityNa
       <FormControl variant={variant} fullWidth>
         <InputLabel>{label}</InputLabel>
         <Select name={name} value={value} onChange={onChange}>
-          {Array.isArray(entityData) &&
-            entityData.map((e) => (
-              <MenuItem key={e.id} value={e.name}>
-                {e.name}
-              </MenuItem>
-            ))}
+          {entityData.map((e) => (
+            <MenuItem key={e.id} value={e.name}>
+              {e.name}
+            </MenuItem>
+          ))}
         </Select>
       </FormControl>
     </>
