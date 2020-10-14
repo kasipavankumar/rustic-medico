f6faf044073e86ae4db25794b7fd4e900154e396
Code Plus Coffee <m416a@protonmail.com>
2020-10-14 18:01:30 +0530

    chore: add utility function to capitalize.

--- admin-client/source/utils/index.ts
+++ admin-client/source/utils/index.ts
@@ -1,2 +1,8 @@
-export { default as fetchEntities } from './fetchEntities';
-export { default as postEntityData } from './postEntityData';
+import fetchEntities from './fetchEntities';
+import postEntityData from './postEntityData';
+
+export function capitalizeFirstLetter(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export { fetchEntities, postEntityData };
