a998fcf278ac1571e128a38c62de43e0396cafb6
D. Kasi Pavan Kumar <44864604+kasipavankumar@users.noreply.github.com>
2022-03-29 19:05:10 +0530

    Use SSL.

--- server/source/database/index.js
+++ server/source/database/index.js
@@ -1,6 +1,11 @@
-const Pool = require('pg').Pool;
-const { DATABASE_URL } = require('../utils/config');
+const Pool = require("pg").Pool;
+const { DATABASE_URL } = require("../utils/config");
 
-const pool = new Pool({ connectionString: DATABASE_URL });
+const pool = new Pool({
+  connectionString: DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = pool;
